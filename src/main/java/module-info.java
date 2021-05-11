119950ad1140194fdf191a13d23f4c5cc4b9e7be
Mathias Jørgensen <mathijo@stud.ntnu.no>
2021-05-11 14:48:58 +0200

    Added files from another project to make this one work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,4 @@ module ntnu.idatx2001 {
 
     opens ntnu.idatx2001 to javafx.fxml;
     exports ntnu.idatx2001;
-    exports ntnu.idatx2001.Model;
-    opens ntnu.idatx2001.Model to javafx.fxml;
-    exports ntnu.idatx2001.ui.Controllers;
-    opens ntnu.idatx2001.ui.Controllers to javafx.fxml;
-    exports ntnu.idatx2001.ui.views;
-    opens ntnu.idatx2001.ui.views to javafx.fxml;
 }
